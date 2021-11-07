8de3ad1c2d14ca2b20747232fd051d9131fcfd7d
Matthew Rubino <83928138+mattrrubino@users.noreply.github.com>
2021-11-06 20:06:12 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, abort
 from flask_cors import CORS
+from twilio.rest import Client
+import os
+import threading
 import socket
+import time
 
 
 app = Flask(__name__)
@@ -11,12 +15,42 @@ IP = '192.168.1.252'
 PORT = 12345
 MESSAGE = 'R'.encode()
 
+# Initialize Twilio API
+ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
+AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
+PHONE_NUMBER = '+13375610964'
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+COOK_TIME = 300
+
+
+def ramen_response(from_, to):
+    client.messages.create(
+        body='Your ramen is being prepared.',
+        from_=from_,
+        to=to
+    )
+
+
+def ramen_started(from_, to):
+    ramen_response(from_, to)
+
+    time.sleep(COOK_TIME)
+
+    client.messages.create(
+        body='Your ramen is ready. Enjoy!',
+        from_=from_,
+        to=to
+    )
+
 
 @app.route('/ramen', methods=['GET', 'POST'])
 def ramen():
     body = request.form['Body']
+    from_ = request.form['From']
+    to = request.form['To']
 
-    if body is None:
+    if body is None or from_ is None or to is None:
         abort(400)
 
     if (not 'ramen' in body.lower()):
@@ -25,6 +59,9 @@ def ramen():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((IP, PORT))
         s.send(MESSAGE)
+
+    thread = threading.Thread(target=ramen_started, args=(to, from_))
+    thread.start()
 
     return "Ramen ordered."
  
